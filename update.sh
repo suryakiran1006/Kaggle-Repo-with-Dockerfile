052ecd090cb0f954bbc3947a413a37ab8255fe8e
kusur <kusur@users.noreply.github.com>
2021-08-23 23:34:06 +0530

    Create update.sh
    Update CentOS and installing Git, Docker Engine, Docker Compose

---
+++ update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Update CentOS
+yum -y update
+
+# Install Git
+yum install -y git
+
+# Install Docker Engine
+yum install -y yum-utils
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install -y docker-ce docker-ce-cli containerd.io
+
+# Install Docker Compose
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
