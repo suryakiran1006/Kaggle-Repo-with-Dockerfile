c1a708250f4cfcb0df93ed63ce5b928f18bce8fc
Kunal Suri <kunal.suri.aws.root@gmail.com>
2021-08-23 23:57:38 +0530

    Added funcationality to start docker service

--- update.sh
+++ update.sh
@@ -10,6 +10,7 @@ yum install -y git
 yum install -y yum-utils
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum install -y docker-ce docker-ce-cli containerd.io
+systemctl start docker
 
 # Install Docker Compose
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
